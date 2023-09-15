963ba0b32bd30bbb709c5c1f3666092fadeb4409
Nimrod Astarhan <nimast@users.noreply.github.com>
2023-09-15 20:25:55 +0000

    add mediaEvent boilerplate

--- sketch.js
+++ sketch.js
@@ -3,6 +3,22 @@ let images = ["1.gif", "2.png"];
 let media = [];
 let texts = ["dream.txt"];
 
+
+
+// class Dream {
+//   MediaEvent[] medieEvents;
+//   getMediaEvents(int timeStamp) : MediaEvent[] {
+
+//   }
+// }
+
+// class MediaEvent {
+//   double startTime;
+//   double endTime;
+//   MediaType type;
+//   URL url;
+// }
+
 function preload() {
   let index = 0;
   for (let i of images) {
@@ -10,7 +26,7 @@ function preload() {
     index++;
   }
   
-  for (let t of texts{
+  for (let t of texts) {
     media[index] = loadStrings("/assets/dream.txt");
     index++;
   }
@@ -24,7 +40,14 @@ function setup() {
 }
 
 function draw() {
-  
+  const mediaEvent = {
+    startTime: 0,
+    endTime: 1200,
+    url: "1.gif",
+    type: "image"
+  };
+
+
 }
 
 function generateDream() {
